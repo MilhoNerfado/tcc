8d2e4eecaddc1166254b4b9c0e61a9f0c5d0140f
Emilio Bottoni <emilio.bottoni@edge.ufal.br>
2024-08-01 16:04:11 -0300

    chore: project startup

---
+++ actuator/drivers/blink/gpio_led.c
@@ -0,0 +1,4 @@
+//
+// Created by milho on 8/1/24.
+//
+#include <zephyr/device.h>
---
+++ actuator/include/app/drivers/blink.h
@@ -0,0 +1,10 @@
+//
+// Created by milho on 8/1/24.
+//
+
+#ifndef BLINK_H
+#define BLINK_H
+
+#include <zephyr/device.h>
+
+#endif //BLINK_H
