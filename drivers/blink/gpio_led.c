b076c6adb43ddfc16cc8759aedc87401d3313189
Emilio Bottoni <emilio.bottoni@edge.ufal.br>
2024-08-02 11:17:25 -0300

    refactor: make 1 app for each application (shared drivers/libs)

--- drivers/blink/gpio_led.c
+++ drivers/blink/gpio_led.c
@@ -1,4 +1,11 @@
 //
 // Created by milho on 8/1/24.
 //
-#include <zephyr/device.h>
+
+#include <zephyr/sys/printk.h>
+
+#include <app/drivers/blink.h>
+
+void blink(void) {
+    printk("Hello from blink");
+}
