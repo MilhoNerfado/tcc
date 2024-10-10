fa846b3a4745e143ba25c2b2f029d93236f05616
Emilio Bottoni <emilio.bottoni@edge.ufal.br>
2024-10-10 09:50:40 -0300

    WIP: Move receiving msg to thread

--- drivers/lora_tcp/lora_tcp_net.c
+++ drivers/lora_tcp/lora_tcp_net.c
@@ -20,12 +20,14 @@ LOG_MODULE_REGISTER(lora_tcp_net);
 K_MSGQ_DEFINE(ack_msgq, sizeof(struct lora_tcp_packet), 10, 4);
 
 K_MSGQ_DEFINE(send_msgq, sizeof(struct lora_tcp_packet), 10, 4);
+K_MSGQ_DEFINE(recv_msgq, sizeof(struct lora_tcp_packet), 10, 4);
 
 static struct lora_modem_config config;
 static const struct device *lora_dev;
+static lora_tcp_cb callback;
 
 static void recv_cb(const struct device *dev, uint8_t *data, uint16_t size, int16_t rssi,
-		    int8_t snr);
+                    int8_t snr);
 
 int lora_tcp_net_init(const struct device *dev)
 {
@@ -60,10 +62,12 @@ int lora_tcp_net_init(const struct device *dev)
 
 	return 0;
 }
+
 static void recv_cb(const struct device *dev, uint8_t *data, uint16_t size, int16_t rssi,
-		    int8_t snr)
+                    int8_t snr)
 {
 	struct lora_tcp_packet packet;
+	struct lora_tcp_packet send_packet;
 
 	LOG_INF("Received data from device | data_len: %u", size);
 
@@ -72,8 +76,8 @@ static void recv_cb(const struct device *dev, uint8_t *data, uint16_t size, int1
 	lora_tcp_packet_unpack(data, size, &packet);
 
 	LOG_INF("send_id: %u | dest_id: %u | crc: %u | pkt_id: %u",
-		packet.header.sender_id, packet.header.destination_id, packet.header.crc,
-		packet.header.pkt_id);
+	        packet.header.sender_id, packet.header.destination_id, packet.header.crc,
+	        packet.header.pkt_id);
 
 	if (packet.header.destination_id != lora_tcp_device_self_get()->id) {
 		return;
@@ -81,11 +85,9 @@ static void recv_cb(const struct device *dev, uint8_t *data, uint16_t size, int1
 
 	if (packet.header.crc != crc32_ieee(packet.data, packet.data_len)) {
 		LOG_ERR("CRC mismatch | expected: %d got: %d",
-			crc32_ieee(packet.data, packet.data_len), packet.header.crc);
+		        crc32_ieee(packet.data, packet.data_len), packet.header.crc);
 		return;
 	}
-
-
 
 	if (packet.header.is_ack) {
 		LOG_WRN("Received ACK");
@@ -93,13 +95,7 @@ static void recv_cb(const struct device *dev, uint8_t *data, uint16_t size, int1
 		return;
 	}
 
-	packet.header.is_ack = true;
-	packet.header.destination_id = packet.header.destination_id ^ packet.header.sender_id;
-	packet.header.sender_id = packet.header.destination_id ^ packet.header.sender_id;
-	packet.header.destination_id = packet.header.destination_id ^ packet.header.sender_id;
-
-	k_msgq_put(&send_msgq, &packet, K_FOREVER);
-	LOG_INF("Sent to send queue");
+	k_msgq_put(&recv_msgq, &packet, K_FOREVER);
 }
 
 int lora_tcp_net_send(struct lora_tcp_packet *pkt, uint8_t *rsp, size_t *rsp_len)
@@ -186,5 +182,29 @@ void send_thread(void *, void *, void *)
 	}
 }
 
+void recv_thread(void *, void *, void *)
+{
+	struct lora_tcp_packet packet;
+
+	while (true) {
+		if (k_msgq_get(&recv_msgq, &packet, K_FOREVER) != 0) {
+			LOG_ERR("Failed to receive packet from device");
+		}
+
+		packet.header.is_ack = true;
+		packet.header.destination_id =
+			packet.header.destination_id ^ packet.header.sender_id;
+		packet.header.sender_id = packet.header.destination_id ^ packet.header.sender_id;
+		packet.header.destination_id =
+			packet.header.destination_id ^ packet.header.sender_id;
+
+		k_msgq_put(&send_msgq, &packet, K_FOREVER);
+		LOG_INF("Sent to send queue");
+	}
+}
+
 K_THREAD_DEFINE(send_tid, CONFIG_LORA_TCP_NET_THREAD_STACK_SIZE, send_thread, NULL, NULL, NULL, 5,
-		0, 0);
+                0, 0);
+
+K_THREAD_DEFINE(recv_tid, CONFIG_LORA_TCP_NET_THREAD_STACK_SIZE, send_thread, NULL, NULL, NULL, 5,
+                0, 0);
--- drivers/lora_tcp/lora_tcp_net.h
+++ drivers/lora_tcp/lora_tcp_net.h
@@ -9,6 +9,8 @@
 
 #include "lora_tcp_packet.h"
 
+typedef void (* lora_tcp_cb)(uint8_t sender_id, uint8_t *data, size_t lenght, uint8_t *response, size_t *response_len);
+
 int lora_tcp_net_init(const struct device * dev);
 
 int lora_tcp_net_send(struct lora_tcp_packet *pkt, uint8_t *rsp, size_t *rsp_len);
